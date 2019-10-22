3d311aaf9ff8a37ea25de4e5db3c0c5066f9cfba
MudrakEM <56842982+MudrakEM@users.noreply.github.com>
2019-10-22 17:00:26 +0500

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,12 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="js/maskedinput.js"></script>
+    <script type="text/javascript">
+    jQuery(function($){
+        $("#phone").mask("9(999)9999999");
+    });
+</script>
+<script type="text/javascript">
+    jQuery(function($){
+        $("#name").mask("А-я ");
+    });
+</script>
